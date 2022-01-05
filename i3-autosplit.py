27a33bd0ecde89a9c39f52208ee4831c46d6bfd7
aurora-0025 <ritingeorge.d@gmail.com>
2022-01-05 16:45:58 +0530

    commit

--- i3-autosplit.py
+++ i3-autosplit.py
@@ -10,14 +10,12 @@ def on_window(i3, e):
             # set split to horizontal
             i3.command('split horizontal')
         else:
-            # set split to vertical           
+            # set split to vertical          
             i3.command('split vertical')
 
 def on_mode_change(i3, e):
     print(e.current.layout)
 
 # Subscribe to events
-i3.on(Event.WINDOW, on_window)
+i3.on(Event.WINDOW_FOCUS, on_window)
 i3.main()
-
-
